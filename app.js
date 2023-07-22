68de3e7ab620068507b326eb5d7367656ce589e8
ekrat <dilekmehmet123@gmail.com>
2023-07-22 23:40:28 +0300

    html, css and javascript functions are completed.

---
+++ app.js
@@ -0,0 +1,37 @@
+let firstNum;
+let secondNum;
+let operator;
+let displayValue = "0";
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return "Error: Invalid operator";
+  }
+}
