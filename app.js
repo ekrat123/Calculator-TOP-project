8bd69d2bd98c0a979ada3729f59ec2c94b3907cf
ekrat <dilekmehmet123@gmail.com>
2023-07-23 10:16:58 +0300

    Calculator work 4 operator and clear button work

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const display = document.querySelector(".display");
+const container = document.querySelector(".container");
+const numBtn = document.querySelectorAll(".number");
+const opeBtn = container.querySelectorAll(".operator");
+const equalBtn = document.getElementById("equal");
+const clearBtn = document.getElementById("clear");
+
 let firstNum;
 let secondNum;
 let operator;
@@ -35,3 +42,54 @@ function operate(operator, a, b) {
       return "Error: Invalid operator";
   }
 }
+
+function updateDisplay() {
+  let clickedValue = this.textContent;
+  if (display.textContent === "0") {
+    display.textContent = clickedValue;
+  } else {
+    display.textContent += clickedValue;
+  }
+}
+
+numBtn.forEach((btn) => {
+  btn.addEventListener("click", updateDisplay);
+});
+
+function getOperator() {
+  let clickedValue = this.textContent;
+  operator = clickedValue;
+  firstNum = display.textContent;
+  display.textContent += operator;
+}
+
+opeBtn.forEach((btn) => {
+  btn.addEventListener("click", getOperator);
+});
+
+function getSecondNum() {
+  let clickedValue = display.textContent;
+  let operateIndex =
+    clickedValue["+"] ||
+    clickedValue["*"] ||
+    clickedValue["-"] ||
+    clickedValue["/"];
+  return clickedValue.slice(operateIndex + 1);
+}
+
+function getResult() {
+  let clickedValue = this.textContent;
+  secondNum = getSecondNum();
+  display.textContent = operate(operator, firstNum, secondNum);
+}
+
+equalBtn.addEventListener("click", getResult);
+
+function clearAll() {
+  firstNum = "0";
+  secondNum = "0";
+  operator = "";
+  display.textContent = "0";
+}
+
+clearBtn.addEventListener("click", clearAll);
